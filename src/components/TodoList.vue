98a8eb4ecb38c886faff3f7db1e637a64be6547a
JANGHYUNn <kimjh2369@gmail.com>
2020-06-05 23:45:51 +0900

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <ul>
+      <li class="shadow" v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item">
+        <i class="fas fa-check checkBtn" v-bind:class="{checkBtnCompleated: todoItem.compleated}" v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.compleated}">{{ todoItem.item }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length ; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          // console.log(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      // console.log(todoItem, index);
+      localStorage.removeItem(todoItem)
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem) {
+      // console.log(todoItem,index);
+      todoItem.compleated = !todoItem.compleated;
+      // 로컬 스토리지 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0px;
+  text-align: center;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleated {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
